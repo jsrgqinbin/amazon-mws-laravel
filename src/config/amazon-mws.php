9117d4746aa1e0f1b61dbcf4b677723533432fad
Ben <jsrgqinbin@gmail.com>
2016-10-18 22:24:56 +0800

    change key

--- src/config/amazon-mws.php
+++ src/config/amazon-mws.php
@@ -12,60 +12,15 @@ return [
     ],
     //Service URL Base
     'DEFAULT_AMAZON_SERVICE_URL' => 'https://mws.amazonservices.com',
-    'DEFAULT_AWS_ACCESS_KEY_ID' => 'AKIAJVQWLC4KFPCPWX2Q',
-    'DEFAULT_AWS_SECRET_ACCESS_KEY' => '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',
+    'DEFAULT_AWS_ACCESS_KEY_ID' => 'AKIAJVQWLC4KFPCPWX2Q1',
+    'DEFAULT_AWS_SECRET_ACCESS_KEY' => 'fk1izWMQ2mINrwYTgV8LB62tz3WeWbpuxq78Hy1o1',
     'muteLog' => false,
     //亚马逊账号信息
     'accountInfo' => [
         'US' => array(
-            'AWS_ACCESS_KEY_ID' => 'AKIAJVQWLC4KFPCPWX2Q',
-            'AWS_SECRET_ACCESS_KEY' => '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',
+            'AWS_ACCESS_KEY_ID' => 'AKIAJVQWLC4KFPCPWX2Q1',
+            'AWS_SECRET_ACCESS_KEY' => 'fk1izWMQ2mINrwYTgV8LB62tz3WeWbpuxq78Hy1o1',
             'SERVICE_URL' => 'https://mws.amazonservices.com'
-        ),
-        'CA' => array(
-            'AWS_ACCESS_KEY_ID' => 'AKIAJVQWLC4KFPCPWX2Q',
-            'AWS_SECRET_ACCESS_KEY' => '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',
-            'SERVICE_URL' => 'https://mws.amazonservices.ca'
-        ),
-        'UK' => array(
-            'AWS_ACCESS_KEY_ID' => 'AKIAIDQGXF27GICZOW5A',
-            'AWS_SECRET_ACCESS_KEY' => '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',
-            'SERVICE_URL' => 'https://mws-eu.amazonservices.com'
-        ),
-        'CN' => array(
-            'AWS_ACCESS_KEY_ID' => 'AKIAIDQGXF27GICZOW5A',
-            'AWS_SECRET_ACCESS_KEY' => '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',
-            'SERVICE_URL' => 'https://mws-eu.amazonservices.com'
-        ),
-        'DE' => array(
-            'AWS_ACCESS_KEY_ID' => 'AKIAIDQGXF27GICZOW5A',
-            'AWS_SECRET_ACCESS_KEY' => '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',
-            'SERVICE_URL' => 'https://mws-eu.amazonservices.com'
-        ),
-        'IT' => array(
-            'AWS_ACCESS_KEY_ID' => 'AKIAIDQGXF27GICZOW5A',
-            'AWS_SECRET_ACCESS_KEY' => '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',
-            'SERVICE_URL' => 'https://mws-it.amazonservices.com'
-        ),
-        'ES' => array(
-            'AWS_ACCESS_KEY_ID' => 'AKIAIDQGXF27GICZOW5A',
-            'AWS_SECRET_ACCESS_KEY' => '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',
-            'SERVICE_URL' => 'https://mws-eu.amazonservices.com'
-        ),
-        'IN' => array(
-            'AWS_ACCESS_KEY_ID' => 'AKIAIDQGXF27GICZOW5A',
-            'AWS_SECRET_ACCESS_KEY' => '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',
-            'SERVICE_URL' => 'https://mws.amazonservices.in'
-        ),
-        'JP' => array(
-            'AWS_ACCESS_KEY_ID' => 'AKIAIA4DZ45PYXLBRZXA',
-            'AWS_SECRET_ACCESS_KEY' => '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',
-            'SERVICE_URL' => 'https://mws.amazonservices.jp'
-        ),
-        'FR' => array(
-            'AWS_ACCESS_KEY_ID' => 'AKIAIDQGXF27GICZOW5A',
-            'AWS_SECRET_ACCESS_KEY' => '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',
-            'SERVICE_URL' => 'https://mws-eu.amazonservices.com'
-        ),
+        )
     ]
 ];
